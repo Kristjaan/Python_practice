a59250952b62a37371458ee14178439e95ff2031
Kristjaan <greeese@gmail.com>
2019-06-23 13:34:17 +0200

    Updated moods

--- mood.py
+++ mood.py
@@ -3,4 +3,16 @@
 #Homework 7.1: The mood checker
 
 mood = input("Hello!\nHow are you today?\n")
-print(mood)
+#mood = mood
+if mood.lower() == "happy":
+    print("It is great to see you happy!")
+elif mood.lower() == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood.lower() == "excited":
+    print("Great! Have fun! Say hello to Mike for me!")
+elif mood.lower() == "sad":
+    print("Get yourself together!")
+elif mood.lower() == "relaxed":
+    print("Keep chillin', Bro!")
+else:
+    print("I don't recogize this mood")
